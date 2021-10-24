4782d9508a112b87fd98d581baff655a747deee3
minjeongPark41 <pmjabcd@gmail.com>
2021-10-24 23:35:09 +0900

    leetcode125

--- leetcode/leetcode125.py
+++ leetcode/leetcode125.py
@@ -7,11 +7,24 @@
 #     return s == s[::-1]
 
 
-from collections import deque
+# from collections import deque
+
+# def isPalindrome(self, s:str) -> bool:
+#     strs:Deque = collections.deque()
+
+#     for char in s:
+#         if char.isalnum():
+#             strs.append(char.lower())
+
+#     while len(strs)>1:
+#         if strs.pop(0) != strs.pop():
+#             return False
+
+#     return True
+
 
 def isPalindrome(self, s:str) -> bool:
-    strs:Deque = collections.deque()
-
+    strs=[]
     for char in s:
         if char.isalnum():
             strs.append(char.lower())
