6aa6cbddb3bcf42a6c53b25a0cbff61173063557
minjeongPark41 <pmjabcd@gmail.com>
2021-12-15 23:48:00 +0900

    boj3020

--- boj/boj3020.py
+++ boj/boj3020.py
@@ -21,15 +21,15 @@ for data in length:
     count += 1
     bottom[h-data+1] += 1 # h-data까지만 하면 걸리는 부분이 없으니 +1
 
-for i in range(h - 1, 0, -1):
-  top[i] += top[i + 1]
+for i in range(h - 1, 0, -1): #h-1부터 -1까지 -1씩 작아지는거 (지금 h가 7이면, 6부터 -1까지)
+  top[i] += top[i + 1] #top[6] += top[7], top[5] += top[6]... 이런식으로 가는건데 위에서 인덱스 삼아서 개수 세어줬던 걸 뒤에서부터 쭉 누적합 해주는 것
 
 # 그낭 h부터 하면 0~h-1이니 헷갈리니
 for i in range(1, h + 1):
-  bottom[i] += bottom[i-1]
+  bottom[i] += bottom[i-1] 
 
 for i in range(1, h + 1):
   answer[i] = top[i] + bottom[i]
 
-answer = answer[1:]
+answer = answer[1:] #슬라이싱. 시작 index부터 끝까지
 print(min(answer), answer.count(min(answer)))
