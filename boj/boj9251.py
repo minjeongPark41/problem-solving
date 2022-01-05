4620e94700234f5a347053ec73c5291cf6bb5e65
minjeongPark41 <pmjabcd@gmail.com>
2022-01-05 23:47:11 +0900

    boj9251

--- boj/boj9251.py
+++ boj/boj9251.py
@@ -6,7 +6,7 @@ dp = [[0] * len(string_b) for _ in range(len(string_a))]
 
 for i in range(1, len(string_a)):
     for j in range(1, len(string_b)):
-        if string_a[i] == string_b[j]: # 같은거 만났을 때는 LCS 조건에 부합하니까 +1
+        if string_a[i] == string_b[j]: # 같은거 만났을 때는 문제에서 말하는 LCS 조건에 부합하니까 +1
             dp[i][j] = dp[i - 1][j - 1] + 1 
         else: # 만약에 다르다면, 그 이전에 구한(그래서 dp 개념) 최대값을 끌고 와줘야 함
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) 
