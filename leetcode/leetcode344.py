bda7ada303136d4294c6285dedef0d07e2fb9ad1
minjeongPark41 <pmjabcd@gmail.com>
2022-04-06 19:03:37 +0900

    leetcode. 344

--- leetcode/leetcode344.py
+++ leetcode/leetcode344.py
@@ -2,9 +2,21 @@
 #     s.reverse()
 
 
-def reverseString(self, s:list[str]) -> None:
-    left, right = 0, len(s)-1
-    while left < right:
-        s[left], s[right] = s[right], s[left]
-        left += 1
-        right -= 1
+# def reverseString(self, s:list[str]) -> None:
+#     left, right = 0, len(s)-1
+#     while left < right:
+#         s[left], s[right] = s[right], s[left]
+#         left += 1
+#         right -= 1
+
+
+
+# 3번째
+
+s = ["h","e","l","l","o"]
+ 
+left, right = 0, len(s)-1
+while left < right:
+    s[left], s[right] = s[right], s[left]
+    left +=1
+    right -+1
