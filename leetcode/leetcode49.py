d1fcbe220fc7107f4b4783b9cd4f4f8a9d453754
minjeongPark41 <pmjabcd@gmail.com>
2022-04-12 23:04:04 +0900

    leetcode. 94

--- leetcode/leetcode49.py
+++ leetcode/leetcode49.py
@@ -1,10 +1,20 @@
 import collections
 
-def groupAnagrams(self, strs):
-    anagrams = collections.defaultdict(list) # 디폴트값을 list로 줌
+# def groupAnagrams(self, strs):
+#     anagrams = collections.defaultdict(list) # 디폴트값을 list로 줌
 
-    for word in strs:
-        # 정렬하여 딕셔너리에 추가
-        anagrams[''.join(sorted(word))].append(word)
+#     for word in strs:
+#         # 정렬하여 딕셔너리에 추가
+#         anagrams[''.join(sorted(word))].append(word)
 
-    return list(anagrams.values())
+#     return list(anagrams.values())
+
+# defaultdict 설명 참고 : https://scribblinganything.tistory.com/12
+
+# 다시
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return anagrams.values()
